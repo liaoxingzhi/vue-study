5afa4c237bf52fa6a8c21c5afdb2c6efb193f501
lxz <18007360791@163.com>
2022-01-14 19:22:51 +0800

    typescript test

---
+++ 01TsHelloWorld/helloworld.js
@@ -0,0 +1,7 @@
+(function () {
+    function hello(msg) {
+        return msg + 'test01';
+    }
+    var msg = 'hello';
+    console.log(hello(msg));
+})();
---
+++ 01TsHelloWorld/helloworld.ts
@@ -0,0 +1,9 @@
+(() => {
+    function hello(msg: string) {
+        return msg + 'test01'
+    }
+
+    let msg: string = 'hello'
+
+    console.log(hello(msg))
+})()
