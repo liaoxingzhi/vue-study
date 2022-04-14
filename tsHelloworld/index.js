62986162f8223720699e280732a7af334e14ee77
lxz <18007360791@163.com>
2022-04-14 14:45:11 +0800

    初识vue2 创建vue实例并渲染数据

--- tsHelloworld/index.js
+++
@@ -1,7 +0,0 @@
-(function () {
-    var name = "啊啊啊";
-    function say(name) {
-        return name + "嗯嗯嗯";
-    }
-    console.log(say(name));
-})();
--- tsHelloworld/index.ts
+++
@@ -1,9 +0,0 @@
-(() => {
-    let name = "hello"
-
-    function say(name:string) {
-        return name + "world"
-    }
-
-    console.log(say(name))
-})()
