43c4dc6749e609a4978efd3ef2f48ed73024b836
liaoxingzhi <42024275+liaoxingzhi@users.noreply.github.com>
2022-05-30 22:08:17 +0800

    vue2脚手架 render函数：将h创建的Node节点信息return返回给Vue.js底层处理文件中的beforeMount()生命周期钩子函数，让其将Node节点信息在界面中渲染出来

--- vue2脚手架/vue2_test/src/main.js
+++ vue2脚手架/vue2_test/src/main.js
@@ -8,6 +8,6 @@ Vue.config.productionTip = false
 
 // 创建Vue实例对象（vm）
 new Vue({
-  //
+  //将h创建的Node节点信息return返回给Vue.js底层处理文件中的beforeMount()生命周期钩子函数，让其将Node节点信息在界面中渲染出来
   render: h => h(App),
 }).$mount('#app') // Vue实例的$mount()方法，作用和el属性完全一样
