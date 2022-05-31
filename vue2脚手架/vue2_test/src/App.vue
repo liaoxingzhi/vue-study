0cedc8a44f42a882d3df90f9cd2672b1d723ee71
lxz <18007360791@163.com>
2022-05-31 15:54:37 +0800

    vue2脚手架 ref标签属性 被用来给元素或子组件注册引用信息（id的替代者）

--- vue2脚手架/vue2_test/src/App.vue
+++ vue2脚手架/vue2_test/src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <School></School>
+    <!-- 被用来给元素或子组件注册引用信息（id的替代者） -->
+    <img ref="img" alt="Vue logo" src="./assets/logo.png">
+    <School ref="school"></School>
     <Student></Student>
+    <button @click="consoleDom">点击打印dom</button>
   </div>
 </template>
 
@@ -15,6 +17,13 @@ export default {
   components: {
     School,
     Student
+  },
+  methods: {
+    consoleDom() {
+      //console.log(this.$refs) 一个对象，持有注册过 ref attribute 的所有 DOM 元素和组件实例
+      console.log(this.$refs.img) //非组件 DOM元素
+      console.log(this.$refs.school) //组件 组件实例
+    }
   }
 }
 </script>
